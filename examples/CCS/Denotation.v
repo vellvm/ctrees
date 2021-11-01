8076bc86ff5d7058306708a65545dbf4092b4ee9
Yannick Zakowski <yannick.zakowski@inria.fr>
2021-11-01 19:20:15 +0100

    WIP on ccs, moving to its own folder

--- examples/CCS/Denotation.v
+++ examples/CCS/Denotation.v
@@ -4,67 +4,25 @@ From Coq Require Export
   List
   Strings.String.
 
-From ITree Require Import
-	Basics.Basics
-	Core.Subevent
-	Interp.Handler
-	Indexed.Sum
-	Events.Exception.
+From ITree Require Import ITree.
 
 From CTree Require Import
 	Utils
 	CTrees
  	Interp
+	Equ
 	Bisim.
+
+From CTreeCCS Require Import 
+	Syntax.
+
+From Coinduction Require Import
+	coinduction rel tactics.
 
 Import CTreeNotations.
 Open Scope ctree_scope.
 
 (* end hide *)
-
-(*
- Sequentially consistent interleaving of computations?
- Definition SC {E} (u v : ctree E unit) : ctree E unit.
-*)
-(* Synchronization vectors? *)
-
-(* Synchronizing zipper? *)
-(* Definition zip {E F} (u : ctree E unit) (v : ctree (dual E) unit) : ctree void1 (option unit) *)
-(* Definition zip {E F} (u : ctree E unit) (v : ctree F unit) : ctree (E *' F) unit *)
-
-(* Paul's yield approach over stateT st (ctree E) *)
-
-Section Channels.
-
-	Definition chan : Set := string.
-
-	Variant action : Type :=
-   | Send (c : chan) : action
-   | Rcv (c : chan) : action.
-
-	Definition op (a : action) : action :=
-		match a with
-		| Send c => Rcv c
-		| Rcv c => Send c
-		end.
-
-	Definition eqb_action : action -> action -> bool :=
-		fun a b => match a,b with
-			| Send c, Send c'
-			| Rcv c, Rcv c' => String.eqb c c'
-			| _, _ => false
-		end .
-
-	Lemma eqb_action_refl : forall a,
-		 eqb_action a a = true.
-	Proof.
-		intros []; cbn; auto using eqb_refl.
-	Qed.
-
-	Definition are_opposite (a b : action) : bool :=
-		if eqb_action a (op b) then true else false.
-
-End Channels.
 
 (* Event signature *)
 
@@ -72,9 +30,11 @@ Variant ActionE : Type -> Type :=
 	| Act (a : action) : ActionE unit.
 Variant SynchE : Type -> Type := Tau : SynchE unit.
 
-Definition DeadE := exceptE unit.
+Variant DeadE : Type -> Type := 
+	| throw : DeadE void.
+
 Definition dead {A : Type} {E} `{DeadE -< E} : ctree E A :=
-	x <- trigger (Throw tt);; match x: void with end.
+	x <- trigger throw;; match x: void with end.
 
 Definition ccsE : Type -> Type := (ActionE +' SynchE +' DeadE).
 
@@ -86,7 +46,7 @@ Section Combinators.
 
 	Definition done : ccs := Ret tt. (* Or should it be Choice 0 ? *)
 
-	Definition prefix (a : action) : ccs := trigger (Act a).
+	Definition prefix (a : action) (P: ccs) : ccs := trigger (Act a);; P.
 
 	Definition plus (P Q : ccs) : ccs := Sanity.choice2 P Q.
 
@@ -185,9 +145,87 @@ Section Combinators.
 
 End Combinators.
 
+Import CCSNotations.
+Open Scope ccs_scope.
+
 (* fun P Q => bisim (model P) (model Q): is this weak bisimulation of CCS?
 
    -> : term -> term -> Prop
    -ccs> : ccs -> ccs -> Prop as
    -sem> : term -> term -> Prop := fun P Q => model P -ccs> model Q
 *)
+
+Fixpoint model (t : term) : ccs :=
+	match t with 
+	| 0     => done
+	| a ⋅ P => prefix a (model P)
+	| P ∥ Q => para (model P) (model Q)
+	| P ⊕ Q => plus (model P) (model Q)
+	| P ∖ c => restrict c (model P)
+	end.
+		
+Module DenNotations.
+
+  (* Notations for patterns *)
+  Notation "'actP' e" := (inl1 e) (at level 10).
+  Notation "'synchP' e" := (inr1 (inl1 e)) (at level 10).
+  Notation "'deadP' e" :=  (inr1 (inr1 e)) (at level 10).
+
+	(* Notation step_ccs := step. *)
+
+  Notation "⟦ t ⟧" := (model t).
+  (* Notation "P '⊢' a '→ccs' Q" := (step_ccs P a Q) (at level 50). *)
+
+End DenNotations.
+
+Import DenNotations.
+
+Notation "pf ↦ k" := (eq_rect_r (fun T => T -> ccs) k pf tt) (at level 40, k at next level).
+Notation get_hd_ P :=
+  match observe P with
+  | RetF x => Ret Hdone
+  | @VisF _ _ _ T e k =>
+  	match e  with
+		| actP e   => match e in ActionE X return X = T -> ccsT head with
+									| Act a => fun pf => (Ret (Hact (Some a) (pf ↦ k)))
+									end eq_refl
+		| synchP e => match e in SynchE X return X = T -> ccsT head with
+									| Tau => fun pf => (Ret (Hact None (pf ↦ k)))
+									end eq_refl
+		| deadP e  => dead
+		end
+  | ChoiceF n k => Choice n (fun i => get_hd (k i))
+  end.
+
+Lemma get_hd_unfold : forall P,
+    get_hd P ≅ get_hd_ P.
+Proof.
+  intros.
+	now step.
+Qed.
+
+Notation para_ P Q :=
+			(rP <- get_hd P;;
+			rQ <- get_hd Q;;
+			match rP, rQ with
+			| Hdone, Hdone => done
+			| Hdone, _ => Q
+			| _, Hdone => P
+			| Hact a P', Hact b Q' =>
+				match a, b with
+				| Some a, Some b =>
+					if are_opposite a b
+					then Sanity.choice3 (vis Tau (fun _ => para P' Q')) (vis (Act a) (fun _ => para P' Q)) (vis (Act b) (fun _ => para P Q'))
+					else Sanity.choice2 (vis (Act a) (fun _ => para P' Q)) (vis (Act b) (fun _ => para P Q'))
+				| Some a, None => Sanity.choice2 (vis (Act a) (fun _ => para P' Q)) (vis Tau (fun _ => para P Q'))
+				| None, Some b => Sanity.choice2 (vis Tau (fun _ => para P' Q)) (vis (Act b) (fun _ => para P Q'))
+				| None, None =>   Sanity.choice2 (vis Tau (fun _ => para P' Q)) (vis Tau (fun _ => para P Q'))
+				end
+			end)%ctree.
+
+Lemma para_unfold : forall P Q, para P Q ≅ para_ P Q.
+Proof.
+  intros.
+	now step.
+Qed.
+
