bce6a5b10d04c62431e436963fd54006ca89e39d
Yannick Zakowski <yannick.zakowski@inria.fr>
2021-11-10 18:28:59 -0500

    The final challenge must also be directed appropriatly

--- theories/BisimWeak.v
+++ theories/BisimWeak.v
@@ -55,17 +55,29 @@ Section wbisim.
 	bisimulation world to ensuring that they can take a small step
 	emmitting the same event. *)
 
-  Variant matching' (bisim : ctree E R1 -> ctree E R2 -> Prop) :
+  Variant matchingL (bisim : ctree E R1 -> ctree E R2 -> Prop) :
     ctree E R1 -> ctree E R2 -> Prop :=
-  | MatchRet x y (RET : RR x y) :
-     matching' bisim (Ret x) (Ret y)
-  | MatchVis {X} (e : E X) k1 k2 
+  | MatchLRet x y (RET : RR x y) :
+     matchingL bisim (Ret x) (Ret y)
+  | MatchLVis {X} (e : E X) k1 k2 
      (RET : forall v t', 
         internal_refl (k1 v) t' ->
         exists u', internal_refl (k2 v) u' /\ bisim t' u'):
-     matching' bisim (Vis e k1) (Vis e k2)
+     matchingL bisim (Vis e k1) (Vis e k2)
   . 
-  Hint Constructors matching': core.
+  Hint Constructors matchingL: core.
+
+  Variant matchingR (bisim : ctree E R1 -> ctree E R2 -> Prop) :
+    ctree E R1 -> ctree E R2 -> Prop :=
+  | MatchRRet x y (RET : RR x y) :
+     matchingR bisim (Ret x) (Ret y)
+  | MatchRVis {X} (e : E X) k1 k2 
+     (RET : forall v t', 
+        internal_refl (k1 v) t' ->
+        exists u', internal_refl (k2 v) u' /\ bisim t' u'):
+     matchingR bisim (Vis e k1) (Vis e k2)
+  . 
+  Hint Constructors matchingR: core.
 
   Variant wbisimF (wbisim : ctree E R1 -> ctree E R2 -> Prop) :
     ctree E R1 -> ctree E R2 -> Prop :=
@@ -76,9 +88,9 @@ Section wbisim.
             (SIMBI : forall t', internal t t' ->
               exists u', internal_refl u u' /\ wbisim u' t')
             (SIMFE : forall u', schedule u u' ->
-              exists t', schedule t t' /\ matching' wbisim u' t')
+              exists t', schedule t t' /\ matchingL wbisim u' t')
             (SIMBE : forall t', schedule t t' ->
-              exists u', schedule u u' /\ matching' wbisim u' t')
+              exists u', schedule u u' /\ matchingR wbisim u' t')
                :
       wbisimF wbisim u t.
  
@@ -87,16 +99,28 @@ Section wbisim.
   Definition wbisim_ eq : ctree E R1 -> ctree E R2 -> Prop :=
 	fun t1 t2 => bisimF eq (observe t1) (observe t2). *)
 
-  Lemma matching'_mono u v sim sim'
-              (IN : matching' sim u v)
+  Lemma matchingL_mono u v sim sim'
+              (IN : matchingL sim u v)
               (LE : sim <= sim') :
-        matching' sim' u v.
+        matchingL sim' u v.
   Proof.
     inv IN; auto. constructor; intros. edestruct RET as (? & ? & ?); eauto.
     eexists; split; eauto. 
     apply LE; auto.
   Qed.
-  Hint Resolve matching'_mono: core.
+  Hint Resolve matchingL_mono: core.
+
+  Lemma matchingR_mono u v sim sim'
+              (IN : matchingR sim u v)
+              (LE : sim <= sim') :
+        matchingR sim' u v.
+  Proof.
+    inv IN; auto. constructor; intros. edestruct RET as (? & ? & ?); eauto.
+    eexists; split; eauto. 
+    apply LE; auto.
+  Qed.
+  Hint Resolve matchingR_mono: core.
+
 
   Program Definition fwbisim : mon (ctree E R1 -> ctree E R2 -> Prop) := {| body := wbisimF |}.
   Next Obligation.
@@ -126,4 +150,5 @@ Notation "x [≊] y" := (bt_bis eq _ x y) (at level 79).
 
 Arguments wbisim _ _ _ _/.
 #[global] Hint Constructors wbisimF: core.
-#[global] Hint Constructors matching': core.
+#[global] Hint Constructors matchingL: core.
+#[global] Hint Constructors matchingR: core.
