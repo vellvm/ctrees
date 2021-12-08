ed05d39fa9c12b0ef5e42cae25f978ef07a4238d
Yannick Zakowski <yannick.zakowski@inria.fr>
2021-12-08 16:44:46 -0500

    clean up-to equ for sbisim

--- theories/CTreesTheory.v
+++ theories/CTreesTheory.v
@@ -18,81 +18,48 @@ Import CTree.
 Import CTreeNotations.
 Open Scope ctree.
 
+Program Definition equ_clos {E X1 X2} : mon (rel (ctree E X1) (ctree E X2)) :=
+  {| body := fun R t u => exists t' u', t' ≅ t /\ R t u /\ u ≅ u' |}.
+Next Obligation.
+  intros * ?? LE t u (t' & u' & eqt & eqtu & equ).
+  do 2 eexists; repeat split; auto.
+  apply LE; auto.
+Qed.
+
+Lemma equ_clos_sym {E X} : compat converse (@equ_clos E X X).
+Proof.
+  intros R t u (t' & u' & eq1 & eq2 & eq3).
+  exists u', t'; intuition.
+Qed.
+
+Lemma equ_clos_t {E X} : @equ_clos E X X <= st.
+Proof.
+  apply Coinduction, by_Symmetry; [apply equ_clos_sym |].
+  intros R t u (t' & u' & eq1 & [F B] & eq3) l t1 TR; cbn in *.
+  apply F in TR as [u1 TR ?].
+  eexists.
+  eauto.
+  apply (f_Tf sb).
+  do 2 eexists; intuition.
+Qed.
+
 Section bind.
-
-  Definition pointwise {X X' Y Y'} (SS : rel X X')
-    : rel Y Y' -> rel (X -> Y) (X' -> Y') :=
-    fun R k k' => forall x x', SS x x' -> R (k x) (k' x').
-  (* Heterogeneous [pair], todo move to library *)
-  Definition pairH {A B : Type} (x : A) (y : B) : A -> B -> Prop :=
-    fun x' y' => x = x' /\ y = y'.
-
-  Lemma leq_pairH : forall {A B : Type} (x : A) (y : B) (R : rel A B),
-      R x y <-> pairH x y <= R.
-  Proof.
-    firstorder congruence.
-  Qed.
-
-  Section Bind_ctx.
-
-    Context {E: Type -> Type} {X X' Y Y': Type}.
-
-    (* Most general contextualisation function associated to [bind]
-	   Can be read more digestly as, where R is a relation on ctrees (the prefixes of the binds) and S on the continuations:
-		 bind_ctx R S = {(bind t k, bind t' k') | R t t' /\ S k k'}
-
-		 This definition could actually be generalized,
-     the same way the Coinduction library provides generic binary contexts ([binary_ctx]).
-	   *)
-
-    (* The most general context:
-    bind_ctx R S ≜ {(bind x k, bind x' k') | R x x' /\ S k k'}
-     *)
-    Definition bind_ctx
-               (R: rel (ctree E X) (ctree E X'))
-               (S: rel (X -> ctree E Y) (X' -> ctree E Y')):
-      rel (ctree E Y) (ctree E Y') :=
-      sup_all (fun x => sup (R x) (fun x' =>
-                                  sup_all (fun k => sup (S k) (fun k' => pairH (bind x k) (bind x' k'))))).
-
-    (* Two lemmas to interact with [bind_ctx]:
-     - [leq_bind_ctx] specifies relations above the context
-     - [in_bind_ctx] specifies how to populate it *)
-    Lemma leq_bind_ctx:
-      forall R S S', bind_ctx R S <= S' <->
-                  (forall x x', R x x' -> forall k k', S k k' -> S' (bind x k) (bind x' k')).
-    Proof.
-      intros.
-      unfold bind_ctx.
-      setoid_rewrite sup_all_spec.
-      setoid_rewrite sup_spec.
-      setoid_rewrite sup_all_spec.
-      setoid_rewrite sup_spec.
-      setoid_rewrite <-leq_pairH.
-      firstorder.
-    Qed.
-
-    Lemma in_bind_ctx (R S :rel _ _) x x' y y':
-      R x x' -> S y y' -> bind_ctx R S (bind x y) (bind x' y').
-    Proof. intros. now apply ->leq_bind_ctx. Qed.
-    Global Opaque bind_ctx.
-
-  End Bind_ctx.
 
   Section Concrete_Bind_ctx.
 
     Context {E: Type -> Type} {X1 X2 Y1 Y2: Type}.
 
-    (* specialisation to a function acting with [equ] on the bound value, and with the argument (pointwise) on the continuation *)
-    Program Definition bind_ctx_equ SS: mon (rel (ctree E Y1) (ctree E Y2)) :=
-      {|body := fun R => @bind_ctx E X1 X2 Y1 Y2 (equ SS) (pointwise SS R) |}.
+    (* specialisation to a function acting with [sbisim] on the bound value, and with the argument (pointwise) on the continuation *)
+    Program Definition bind_ctx_sbisim : mon (rel (ctree E Y1) (ctree E Y1)) :=
+      {|body := fun R => @bind_ctx E X1 X1 Y1 Y1 sbisim (pointwise eq R) |}.
     Next Obligation.
-      intros ??? H. apply leq_bind_ctx. intros ?? H' ?? H''.
+      intros ?? H. apply leq_bind_ctx. intros ?? H' ?? H''.
       apply in_bind_ctx. apply H'. intros t t' HS. apply H, H'', HS.
     Qed.
 
+
     (* this gives a valid up-to technique *)
-    Lemma bind_ctx_equ_t (SS : rel X1 X2) (RR : rel Y1 Y2): bind_ctx_equ SS <= t_equ RR.
+    Lemma bind_ctx_equ_t (SS : rel X1 X2) (RR : rel Y1 Y2): @bind_ctx_equ E _ _ _ _ SS <= t_equ RR.
     Proof.
       apply Coinduction. intros R. apply (leq_bind_ctx _).
       intros x x' xx' k k' kk'.
@@ -111,24 +78,44 @@ Section bind.
         red; intros. apply (b_T (fequ _)), kk'; auto.
     Qed.
 
-    (* specialisation to a function acting with [sbisim] on the bound value, and with the argument (pointwise) on the continuation *)
-    Program Definition bind_ctx_sbisim : mon (rel (ctree E Y1) (ctree E Y1)) :=
-      {|body := fun R => @bind_ctx E X1 X1 Y1 Y1 sbisim (pointwise eq R) |}.
-    Next Obligation.
-      intros ?? H. apply leq_bind_ctx. intros ?? H' ?? H''.
-      apply in_bind_ctx. apply H'. intros t t' HS. apply H, H'', HS.
+    Lemma bind_ctx_sym: compat converse bind_ctx_sbisim.
+    Proof. intro R. simpl. apply leq_bind_ctx. intros. apply in_bind_ctx.
+           symmetry; auto.
+           intros ? ? ->.
+           apply H0; auto.
     Qed.
 
-    (* Inverting transitions from binds *)
     (* this gives a valid up-to technique *)
     Lemma bind_ctx_sbisim_t : bind_ctx_sbisim <= st.
     Proof.
-      apply Coinduction. intros R. apply (leq_bind_ctx _).
-      intros x x' xx' k k' kk'.
-      step in xx'; destruct xx' as (F & B); cbn in *.
-      split.
-      - cbn; intros l t' STEP. 
-      destruct xx'.
+      apply Coinduction, by_Symmetry.
+      apply bind_ctx_sym.
+      intros R. apply (leq_bind_ctx _).
+      intros t1 t2 tt k1 k2 kk.
+      step in tt; destruct tt as (F & B); cbn in *.
+      cbn in *; intros l u STEP.
+      apply trans_bind_inv in STEP as [(H & t' & STEP & EQ) | ?]; cbn in *.
+      - apply F in STEP as [u' STEP EQ'].
+        eexists.
+        apply trans_bind_l; eauto.
+        apply (@fT_T _ _ sb equ_clos foo).
+
+
+        apply (fTf_Tf sb).
+        eapply (fT_T sb).
+        apply (fTf_Tf sb).
+        apply in_bind_ctx. apply REL.
+        red in kk.
+        generalize (kk _ _ REL).
+        apply id_T.
+        apply in_bind_ctx. apply REL.
+        apply (b_T sb).
+        split.
+        rewrite EQ.
+
+          intros ?. ? ?. exists. cbn. cbv. red. kk'; auto.
+
+      destruct xx' .
       - cbn in *.
         generalize (kk' _ _ REL).
         apply (fequ RR).
@@ -214,5 +201,26 @@ End bind.
     Next Obligation.
       intros ?????? H. apply leq_bind_ctx. intros ?? H' ?? H''.
       apply in_bind_ctx. apply H'. intros t t' HS. apply H, H'', HS.
+    Qed.
+
+    (* this gives a valid up-to technique *)
+    (* research question: is there a meaningful way do deal with bind_ctx in general? *)
+    Lemma bind_ctx_equ_t (SS : rel X X') (RR : rel Y Y'): bind_ctx' SS <= t_equ RR.
+    Proof.
+      apply Coinduction. intros R. apply (leq_bind_ctx _).
+      intros x x' xx' k k' kk'.
+      apply (gfp_pfp (fequ _)) in xx'.
+      cbn; unfold observe; cbn.
+      destruct xx'.
+      - cbn in *.
+        generalize (kk' _ _ REL).
+        apply (fequ RR).
+        apply id_T.
+      - constructor; intros ?. apply (fTf_Tf (fequ _)).
+        apply in_bind_ctx. apply REL.
+        red; intros. apply (b_T (fequ _)), kk'; auto.
+      - constructor. intro a. apply (fTf_Tf (fequ _)).
+        apply in_bind_ctx. apply REL.
+        red; intros. apply (b_T (fequ _)), kk'; auto.
     Qed.
 
