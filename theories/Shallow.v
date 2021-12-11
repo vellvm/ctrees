59212a030406f2413309e5bf36bf9a7cfe99232b
Yannick Zakowski <yannick.zakowski@inria.fr>
2021-12-11 14:31:33 -0500

    Fix warnings

--- theories/Shallow.v
+++ theories/Shallow.v
@@ -77,19 +77,19 @@ Section observing_relations.
 Context {E : Type -> Type} {R : Type}.
 Variable (eq_ : ctree' E R -> ctree' E R -> Prop).
 
-Global Instance observing_observe_ :
+#[global] Instance observing_observe_ :
   Proper (observing eq_ ==> eq_) (@observe E R).
 Proof. intros ? ? []; cbv; auto. Qed.
 
-Global Instance observing_go : Proper (eq_ ==> observing eq_) (@go E R).
+#[global] Instance observing_go : Proper (eq_ ==> observing eq_) (@go E R).
 Proof. cbv; auto. Qed.
 
-Global Instance monotonic_observing eq_' :
+#[global] Instance monotonic_observing eq_' :
   subrelation eq_ eq_' ->
   subrelation (observing eq_) (observing eq_').
 Proof. intros ? ? ? []; cbv; eauto. Qed.
 
-Global Instance Equivalence_observing :
+#[global] Instance Equivalence_observing :
   Equivalence eq_ -> Equivalence (observing eq_).
 Proof.
   intros []; split; cbv; auto.
@@ -101,7 +101,7 @@ End observing_relations.
 
 (** ** Unfolding lemmas for [bind] *)
 
-Instance observing_bind {E R S} :
+#[global] Instance observing_bind {E R S} :
   Proper (observing eq ==> eq ==> observing eq) (@CTree.bind E R S).
 Proof.
   repeat intro; subst. constructor. unfold observe. cbn.
@@ -140,7 +140,7 @@ Proof. econstructor. reflexivity. Qed.
 Inductive going {E R1 R2} (r : ctree E R1 -> ctree E R2 -> Prop)
           (ot1 : ctree' E R1) (ot2 : ctree' E R2) : Prop :=
 | going_intros : r (go ot1) (go ot2) -> going r ot1 ot2.
-Global Hint Constructors going: core.
+#[global] Hint Constructors going: core.
 
 Lemma observing_going {E R1 R2} (eq_ : ctree' E R1 -> ctree' E R2 -> Prop) ot1 ot2 :
   going (observing eq_) ot1 ot2 <-> eq_ ot1 ot2.
@@ -154,15 +154,15 @@ Section going_relations.
 Context {E : Type -> Type} {R : Type}.
 Variable (eq_ : ctree E R -> ctree E R -> Prop).
 
-Global Instance going_go : Proper (going eq_ ==> eq_) (@go E R).
+#[global] Instance going_go : Proper (going eq_ ==> eq_) (@go E R).
 Proof. intros ? ? []; auto. Qed.
 
-Global Instance monotonic_going eq_' :
+#[global] Instance monotonic_going eq_' :
   subrelation eq_ eq_' ->
   subrelation (going eq_) (going eq_').
 Proof. intros ? ? ? []; eauto. Qed.
 
-Global Instance Equivalence_going :
+#[global] Instance Equivalence_going :
   Equivalence eq_ -> Equivalence (going eq_).
 Proof.
   intros []; constructor; cbv; eauto.
