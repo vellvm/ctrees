7d4655ecf7aa49c643207239b0abf8b24dc73e41
Yannick Zakowski <yannick.zakowski@inria.fr>
2022-02-13 20:54:03 +0100

    Embedding of itrees into ctrees are a bit tricky

--- theories/ITree.v
+++ theories/ITree.v
@@ -1,5 +1,5 @@
 From CTree Require Import
-	CTrees Equ Bisim.
+	CTrees Trans Equ Bisim.
 
 From ITree Require Import
 	ITree Eq.
@@ -18,38 +18,33 @@ Definition embed {E X} : itree E X -> ctree E X :=
 	cofix _embed t :=
 	 match observe t with
 	| RetF x => CTrees.Ret x
-	| TauF t => CTrees.TauV (_embed t)
+	| TauF t => CTrees.TauI (_embed t)
 	| VisF e k => CTrees.Vis e (fun x => _embed (k x))
 	 end.
 
 Notation "'_embed' ot" :=
 	(match ot with
 	| RetF x => CTrees.Ret x
-	| TauF t => CTrees.TauV (embed t)
+	| TauF t => CTrees.TauI (embed t)
 	| VisF e k => CTrees.Vis e (fun x => embed (k x))
  end) (at level 50, only parsing).
 
-(* Painfully ad-hoc patch to avoid 30s of unification... *)
-#[global] Instance Reflexive_equ {E R}: Reflexive (gfp (@fequ E R _ eq)).
-Proof. apply Equivalence_equ.  Qed.
-
-Lemma embed_unfold {E X} (t : itree E X) :
+Lemma unfold_embed {E X} (t : itree E X) :
 	equ eq (embed t) (_embed (observe t)).
 Proof.
-	(* Warning, if Bisim is in scope and we don't have the instance above, this takes 30 seconds *)
   now step.
 Qed.
 
-#[local] Notation iobserve := observe.
+#[local] Notation iobserve  := observe.
 #[local] Notation _iobserve := _observe.
-#[local] Notation cobserve := CTrees.observe.
+#[local] Notation cobserve  := CTrees.observe.
 #[local] Notation _cobserve := CTrees._observe.
-#[local] Notation iRet x   := (Ret x).
-#[local] Notation iVis e k := (Vis e k).
-#[local] Notation iTau t   := (Tau t).
-#[local] Notation cRet x   := (CTrees.Ret x).
-#[local] Notation cTau t   := (CTrees.TauI t).
-#[local] Notation cVis e k := (CTrees.Vis e k).
+#[local] Notation iRet x    := (Ret x).
+#[local] Notation iVis e k  := (Vis e k).
+#[local] Notation iTau t    := (Tau t).
+#[local] Notation cRet x    := (CTrees.Ret x).
+#[local] Notation cTauI t   := (CTrees.TauI t).
+#[local] Notation cVis e k  := (CTrees.Vis e k).
 
 Lemma embed_eq {E X}:
 	Proper (eq_itree eq ==> equ eq) (@embed E X).
@@ -57,7 +52,7 @@ Proof.
 	unfold Proper, respectful.
 	coinduction r CIH.
 	intros t u bisim.
-	rewrite 2 embed_unfold.
+	rewrite 2 unfold_embed.
 	punfold bisim.
 	inv bisim; pclearbot; try easy.
 	- constructor; intros _.
@@ -66,10 +61,56 @@ Proof.
 		apply CIH, REL.
 Qed.
 
+From RelationAlgebra Require Import
+     monoid
+     kat
+     kat_tac
+     prop
+     rel
+     srel
+     comparisons
+     rewriting
+     normalisation.
+
+From Coq Require Import Datatypes.
+
+(* This is actually not trivial.
+   There are two ways to encode itrees' taus:
+   - If we use TauI, then I believe we have eutt mapping to sbisim I believe.
+   Proving so however is tricky: [eutt] has a weird behavior that consists of
+   being allowed to either look at taus (tau/tau rule) or ignore them (asymmetric rules).
+   In contrast, [sbisim] can only ignore [TauI]. In the Tau/Tau case, it's therefore quite
+   unclear how the proof should proceed: fundamentally, the rule is useful in [eutt] if and
+   only if the computations are both [spin] from now on --- in all other cases it can be
+   replaced by two asymmetric rules.
+   And as it turns out, if it is indeed [spin] against [spin], then [sbisim] relate [spinI]
+   against itself as well.
+   But how do we turn this into a proof?
+   - If we use TauV, then [eutt] certainly does not map against sbisim --- actually, it maps
+   against [equ] as well in this case. However, I think it should map against [wbisim], but
+   that remains to be proved.
+ *)
 
 Lemma embed_eutt {E X}:
-  Proper (eutt eq ==> wbisim) (@embed E X).
+  Proper (eutt eq ==> sbisim) (@embed E X).
 Proof.
+  unfold Proper,respectful.
+  coinduction ? CIH.
+  symmetric using idtac.
+  - intros * HR * EQ.
+    apply HR; symmetry; assumption.
+  - intros * EUTT.
+    cbn; intros * TR.
+    rewrite unfold_embed in TR.
+    punfold EUTT; red in EUTT.
+    remember (iobserve y) as oy.
+    induction EUTT.
+    + eexists; [rewrite unfold_embed, <- Heqoy; subst; apply TR | reflexivity].
+    + pclearbot.
+      apply CIH in REL.
+      eexists; [rewrite unfold_embed, <- Heqoy |].
+      apply trans_TauI.
+      apply trans_TauI_inv in TR.
 Admitted.
 
 (* Maybe simpler to just write a coinductive relation *)
