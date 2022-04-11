b16c44ab4d2e2386da4e873a852d5d867158fadb
Yannick Zakowski <yannick.zakowski@inria.fr>
2022-04-11 09:37:13 +0200

    comments

--- theories/Eq.v
+++ theories/Eq.v
@@ -1,3 +1,11 @@
+(*|
+Equivalence of computations
+===========================
+This file reexports everything that's necessary to reason w.r.t.
+[equ] (coinductive structural equality) and [sbisim] (strong bisimulation).
+Tactics are redefined locally to support both relations.
+|*)
+
 From Coinduction Require Export
      coinduction rel tactics.
 
@@ -12,6 +20,10 @@ From CTree.Eq Require Export
 Export EquNotations.
 Export SBisimNotations.
 
+(*|
+The [step], [step in] and [coinduction] tactics from [coinduction]
+ with additional unfolding and refolding of [equ] and [sbisim]
+|*)
 #[global] Tactic Notation "step" :=
   __step_equ || __step_sbisim || step.
 
@@ -21,14 +33,34 @@ Export SBisimNotations.
 #[global] Tactic Notation "step" "in" ident(H) :=
   __step_in_equ H || __step_in_sbisim H || step_in H.
 
-#[global] Tactic Notation "upto_vis" := __upto_vis_sbisim.
-
+(*|
+Assuming a goal of the shape [t ~ u], initialize the two challenges
+|*)
 #[global] Tactic Notation "play" := __play_sbisim.
+
+(*|
+Assuming an hypothesis of the shape [t ~ u], extract the forward (playL)
+or backward (playR) challenge --- the e-versions looks for the hypothesis
+|*)
 #[global] Tactic Notation "playL" "in" ident(H) := __playL_sbisim H.
 #[global] Tactic Notation "playR" "in" ident(H) := __playR_sbisim H.
 #[global] Tactic Notation "eplayL" := __eplayL_sbisim.
 #[global] Tactic Notation "eplayR" := __eplayR_sbisim.
 
+(*|
+The upto [Vis] context principle for [sbisim]
+|*)
+#[global] Tactic Notation "upto_vis" := __upto_vis_sbisim.
+
+(*|
+The upto [bind] context principle for [equ] and [sbisim] ---
+the same tactic covers both cases, whether in front of a [gfp], [t _] or [bt _].
+The three variants are:
+- [upto_bind]: leave you with both proof obligations, introducing an evar for the intermediate relation in the case of [equ]
+- [upto_bind_eq]: meant to be use when the prefixes of the computations
+are identical: assumes [reflexivity] will solve the first goal, and proceed to substitute the equality
+- [upto_bind with SS]: for [equ], provides explicitly the intermediate relation
+|*)
 #[global] Tactic Notation "upto_bind" :=
   __eupto_bind_equ || __upto_bind_sbisim.
 
@@ -38,6 +70,10 @@ Export SBisimNotations.
 #[global] Tactic Notation "upto_bind" "with" uconstr(SS) :=
   __upto_bind_equ SS.
 
+(*|
+Weakens equalities into respectively [equ] and [sbisim] equations ---
+useful to setup inductions.
+|*)
 Ltac eq2equ H :=
   match type of H with
   | ?u = ?t => let eq := fresh "EQ" in assert (eq : u ≅ t) by (subst; reflexivity); clear H
