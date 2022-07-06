b88d2206d7fdc9af3e096c376096e0799090bdae
Yannick Zakowski <yannick.zakowski@inria.fr>
2022-07-06 13:41:00 +0200

    Nits in imp

--- examples/Imp/Imp.v
+++ examples/Imp/Imp.v
@@ -14,15 +14,10 @@ From ExtLib Require Import
      Data.List
      Data.Map.FMapAList.
 
-(* Universe issue, TO FIX *)
-Unset Universe Checking.
-Unset Auto Template Polymorphism.
-
 From ITree Require Import
      ITree
      Basics.CategoryKleisli
-     Events.State
-     Events.StateFacts.
+     Events.State.
 
 From Coinduction Require Import
 	coinduction rel tactics.
@@ -38,10 +33,14 @@ Import MonadNotation.
 Local Open Scope monad_scope.
 Local Open Scope string_scope.
 
-(** Imp manipulates a countable set of variables represented as [string]s: *)
+(*|
+Imp manipulates a countable set of variables represented as [string]s:
+|*)
 Definition var : Set := string.
 
-(** For simplicity, the language manipulates [nat]s as values. *)
+(*|
+For simplicity, the language manipulates [nat]s as values.
+|*)
 Definition value : Type := nat.
 
 Inductive expr : Type :=
@@ -51,7 +50,9 @@ Inductive expr : Type :=
 | Minus (_ _ : expr)
 | Mult  (_ _ : expr).
 
-(** Classic Imp plus a non-deterministic branching statement *)
+(*|
+Imp with a non-deterministic branching statement
+|*)
 Inductive stmt : Type :=
 | Assign (x : var) (e : expr)    (* x = e *)
 | Seq    (a b : stmt)            (* a ; b *)
@@ -66,7 +67,7 @@ Variant ImpState : Type -> Type :=
 | GetVar (x : var) : ImpState value
 | SetVar (x : var) (v : value) : ImpState unit.
 
-Section Denote1.
+Section Semantics.
   Fixpoint denote_expr (e : expr) : ctree (stateE value) value :=
     match e with
     | Var v     => trigger (Get _)
@@ -103,61 +104,95 @@ Section Denote1.
 
     end.
 
-  Lemma branch_commut : forall (a b : stmt), denote_imp (Branch a b) ~ denote_imp (Branch b a).
+  Definition interp_imp : ctree (stateE value) ~> Monads.stateT value (ctree void1) :=
+    interp_state (@h_state _ void1).
+
+End Semantics.
+Notation "⟦ c ⟧" := (denote_imp c).
+Notation "'ℑ' c" := (@interp_imp _ (denote_imp c)) (at level 10).
+
+Section Theory.
+
+(*|
+  Given the direct translation of the [Branch] construction, the
+  primitive equational theory that we have at the [ctree] level is
+  straightforwardly lifted to the language level, more specifically
+  at the level of uninterpreted ctrees.
+|*)
+  Lemma branch_commut : forall (a b : stmt),
+      ⟦Branch a b⟧ ~ ⟦Branch b a⟧.
   Proof.
-    intros. cbn. apply choiceI2_commut.
+    intros; apply choiceI2_commut.
   Qed.
 
-  Lemma branch_assoc : forall (a b c : stmt), denote_imp (Branch a (Branch b c)) ~ denote_imp (Branch (Branch a b) c).
+  Lemma branch_assoc : forall (a b c : stmt),
+      ⟦Branch a (Branch b c)⟧ ~ ⟦Branch (Branch a b) c⟧.
   Proof.
-    intros. cbn. rewrite choiceI2_assoc. reflexivity.
-  Qed.
-  
-  Lemma branch_idem : forall a : stmt, denote_imp (Branch a a) ~ denote_imp a.
-  Proof.
-    cbn. intros. apply choiceI2_idem.
+    intros; cbn.
+    now rewrite choiceI2_assoc.
   Qed.
 
-  Lemma branch_block_l : forall a : stmt, denote_imp (Branch Block a) ~ denote_imp a.
+  Lemma branch_idem : forall a : stmt,
+      ⟦Branch a a⟧ ~ ⟦a⟧.
   Proof.
-    cbn. intros. apply choiceI2_stuckI_l.
-  Qed.
-
-  Lemma branch_block_r : forall a : stmt, denote_imp (Branch a Block) ~ denote_imp a.
-  Proof.
-    cbn. intros. apply choiceI2_stuckI_r.
-  Qed.
-
-  Lemma branch_block_r_interp : forall (a : stmt) s,
-    interp_state (@h_state _ void1) (denote_imp (Branch a Block)) s ~
-    interp_state (@h_state _ void1) (denote_imp a) s.
-  Proof.
-    assert (forall X (e : stateE value X) s, vsimple (h_state (E := void1) e s)).
-    { unfold h_state, vsimple. intros. destruct e; eauto. }
-    epose proof interp_state_sbisim (h_state (S := value)) H.
-    intros. rewrite branch_block_r. reflexivity.
+    intros; apply choiceI2_idem.
   Qed.
 
   Lemma branch_congr : forall a a' b b',
-    denote_imp a ~ denote_imp a' ->
-    denote_imp b ~ denote_imp b' ->
-    denote_imp (Branch a b) ~ denote_imp (Branch a' b').
+      ⟦a⟧ ~ ⟦a'⟧ ->
+      ⟦b⟧ ~ ⟦b'⟧ ->
+      ⟦Branch a b⟧ ~ ⟦Branch a' b'⟧.
   Proof.
     intros. cbn. apply sb_choiceI_id.
     intro; destruct x; rewrite ?H, ?H0; reflexivity.
   Qed.
 
-  Example branch_ex : forall s : value,
-    interp_state (@h_state _ void1) (denote_imp (
-      Branch
-        (Seq
-          (Assign "x" (Lit 0))
-          (Assign "x" (Lit 1)))
-        Block
-    )) s ~
-    interp_state (@h_state _ _) (denote_imp (Assign "x" (Lit 1))) s.
+  Lemma branch_block_l : forall a : stmt,
+      ⟦Branch Block a⟧ ~ ⟦a⟧.
   Proof.
-    intros. rewrite branch_block_r_interp.
+    intros; apply choiceI2_stuckI_l.
+  Qed.
+
+  Lemma branch_block_r : forall a : stmt,
+      ⟦Branch a Block⟧ ~ ⟦a⟧.
+  Proof.
+    intros; apply choiceI2_stuckI_r.
+  Qed.
+
+(*|
+  Strong bisimilarity at the uninterpreted level can be transported
+  freely after interpretation via [interp_state_sbisim].
+  For instance here transporting [branch_block_r].
+|*)
+
+  Lemma h_state_is_simple :
+    forall (X : Type) (e : stateE value X) (s : value), vsimple (E := void1) (h_state e s).
+  Proof.
+    unfold h_state, vsimple. intros. destruct e; eauto.
+  Qed.
+
+  Lemma branch_block_r_interp : forall (a : stmt) s,
+    ℑ (Branch a Block) s ~
+    ℑ a s.
+  Proof.
+    epose proof interp_state_sbisim (h_state (S := value)) h_state_is_simple.
+    intros. rewrite branch_block_r. reflexivity.
+  Qed.
+
+(*|
+Finally, we can put bits together to prove that the programs [p3] and [br p2 or p3]
+from Section 2 are indeed equivalent.
+|*)
+  Example branch_ex : forall s : value,
+      ℑ (Branch
+           (Seq
+              (Assign "x" (Lit 0))
+              (Assign "x" (Lit 1)))
+           Block) s ~
+        ℑ (Assign "x" (Lit 1)) s.
+  Proof with (unfold interp_imp).
+    intros...
+    rewrite branch_block_r_interp...
     rewrite 2 unfold_interp_state.
     cbn. setoid_rewrite sb_tauI.
     setoid_rewrite bind_ret_l. rewrite interp_state_ret.
@@ -173,4 +208,4 @@ Section Denote1.
       now rewrite EQ.
   Qed.
 
-End Denote1.
+End Theory.
