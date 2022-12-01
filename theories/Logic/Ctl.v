808b8b170df141069fce48c5f255366f2ec3c8e2
Lef Ioannidis <elefthei@seas.upenn.edu>
2022-12-01 02:23:34 -0500

    Congruences for CTL

--- theories/Logic/Ctl.v
+++ theories/Logic/Ctl.v
@@ -31,6 +31,10 @@ Set Implicit Arguments.
 (* TODO: Decide where to set this *)
 Arguments trans : simpl never.
 
+(* Bounds *)
+Notation Top := (fun _ _ => True).
+Notation Bot := (fun _ _ => False).
+
 Section Ctl.
   Arguments label: clear implicits.
   Context {E C: Type -> Type} {X: Type} {HasStuck: B0 -< C}.
@@ -39,7 +43,7 @@ Section Ctl.
 
   (* Lift label predicates to SP *)
   Definition lift(p: label E -> Prop): SP :=
-    fun t l => p l.
+    fun _ l => p l.
   
   (* Propositional *)
   Definition cimpl: SP -> SP -> SP :=
@@ -63,26 +67,35 @@ Section Ctl.
   (* Exists Next *)
   Definition ex: SP -> SP :=
     fun p t _ => exists l t', trans l t t' -> p t' l.
+
+  (* Forall strong until *)
+  Inductive au: SP -> SP -> SP :=
+  | MatchA: forall t l (p q: SP),      (* Match state [t] and label [l] *)
+      q t l -> au p q t l
+  | StepA: forall t l (p q: SP),       (* Matches all next states [t'] *)
+      p t l ->
+      (forall l' t', trans l' t t' ->                
+                au p q t' l') ->
+      au p q t l.
+
+  (* Exists strong until *)
+  Inductive eu: SP -> SP -> SP :=
+  | MatchE: forall t l (p q: SP),      (* Match state [t] and label [l] *)
+      q t l -> eu p q t l
+  | StepE: forall t l (p q: SP),       (* Matches one next state [t'] *)
+      p t l ->
+      (exists l' t', trans l' t t' ->                
+                eu p q t' l') ->
+      eu p q t l.
   
-  (* Forall Finally *)
-  Inductive af: SP -> SP :=
-  | MatchA: forall t l (p: SP),      (* Match state [t] and label [l] *)
-      p t l -> af p t l
-  | StepA: forall t l (p: SP),       (* Matches all next states [t'] *)
-      (forall l' t', trans l' t t' ->
-               af p t' l') ->
-      af p t l.
-
+  (* Forall finally *)
+  Definition af p := (au Top p).
+  
   (* Exists finally *)
-  Inductive ef: SP -> SP :=
-  | MatchE: forall t l (p: SP),        (* Match state [t] and label [l] *)
-      p t l -> ef p t l
-  | StepE: forall t l (p: SP),         (* Matches one next state [t'] *)
-      (exists l' t', trans l' t t' ->
-                ef p t' l') ->
-      ef p t l.
+  Definition ef p := (eu Top p).
 
-  (* Coinductive -- Forall global *)
+  (*| Coinductives| *)  
+  (* Forall global *)
   Definition agF (R: SP -> SP): SP -> SP :=
     fun (p: SP) t l =>
       p t l /\
@@ -94,8 +107,8 @@ Section Ctl.
       p t l /\
       (exists t' l', trans l' t t' -> R p t' l').
 
-  Hint Constructors ef af: core.
-  Hint Unfold lift cand cor cimpl cnot ax ex agF egF: core.
+  Hint Constructors eu au: core.
+  Hint Unfold lift cand cor cimpl cnot af ef ax ex agF egF: core.
     
   Program Definition ag_: mon (SP -> SP) := {| body := agF  |}.
   Next Obligation.
@@ -114,9 +127,6 @@ End Ctl.
 
 Module CtlNotations.
 
-  Notation Top := (fun _ _ => True).
-  Notation Bot := (fun _ _ => False).
-
   Notation "t , l |= p" := (p t l) (at level 90, left associativity, only parsing).
   
   Notation "'EX' p" := (ex p) (at level 70).
@@ -125,10 +135,12 @@ Module CtlNotations.
   Notation "'AF' p" := (af p) (at level 70).
   Notation "'EG' p" := (eg p) (at level 70).
   Notation "'AG' p" := (ag p) (at level 70).
+  Notation "p 'AU' q" := (au p q) (at level 50, left associativity).
+  Notation "p 'EU' q" := (eu p q) (at level 50, left associativity). 
 
   Notation "p '&&&' q" := (cand p q) (at level 89, left associativity).
   Notation "p '|||' q" := (cor p q) (at level 89, left associativity).
-  Notation "p '->>' q" := (cimpl p q) (at level 89, left associativity).
+  Notation "p '->>' q" := (cimpl p q) (at level 99, right associativity).
   Notation "'!' p" := (cnot p) (at level 89).
   
   Notation "[ 'EG' ] p" := (eg_ p) (at level 40).
@@ -207,7 +219,7 @@ Section Lemmas.
     apply CONTRA.    
   Qed.
 
-  (*| Ex-falso *)
+  (*| Ex-falso |*)
   Lemma ctl_ex_falso: forall p,
       t,l |= Bot -> t,l |= p.
   Proof.
@@ -224,7 +236,7 @@ Section Lemmas.
       A[φ U ψ] ≡ ψ ∨ (φ ∧ AX A[φ U ψ])
       E[φ U ψ] ≡ ψ ∨ (φ ∧ EX E[φ U ψ])
    *)
-  Lemma af_ax: forall p,
+  Lemma ctl_af_ax: forall p,
       t,l |= AF p <-> t,l |= p ||| AX (AF p).
   Proof.
     split; intros; inv H.
@@ -234,7 +246,7 @@ Section Lemmas.
     - now right.
   Qed.
 
-  Lemma ef_ex: forall p,
+  Lemma ctl_ef_ex: forall p,
       t,l |= EF p <-> t,l |= p ||| EX (EF p).
   Proof.
     split; intros; inv H.
@@ -244,19 +256,121 @@ Section Lemmas.
     - now right.
   Qed.
 
-  Lemma ag_ax: forall p,
+  Lemma ctl_ag_ax: forall p,
       t,l |= AG p <-> t,l |= p &&& AX (AG p).
-  Proof with auto.
+  Proof. 
     split; intros.
     - split; step in H.
       + now destruct H.
       + destruct H.
-        unfold ax...
+        unfold ax; auto.
     - destruct H.
       red in H0.
       step.
-      unfold ag_, agF; split; eauto; simpl.
+      split; eauto; simpl.
       intros.
-  Admitted.
+      now apply H0.
+  Qed.
 
+  Lemma ctl_eg_ex: forall p,
+      t,l |= EG p <-> t,l |= p &&& EX (EG p).
+  Proof. 
+    split; intros.
+    - split; step in H.
+      + now destruct H.
+      + destruct H as (? & t' & l' & ?).
+        unfold ex; eauto.
+    - destruct H as (? & t' & l' & ?).
+      step.
+      split; eauto.
+  Qed.
+
+  Lemma ctl_au_ax: forall p q,
+      t,l |= p AU q <-> t,l |= q ||| (p &&& AX (p AU q)).
+  Proof.
+    split; intros.
+    - inv H.
+      + now left.
+      + now right.
+    - inv H.
+      + now apply MatchA.
+      + destruct H0.
+        apply StepA; auto.
+  Qed.
+                   
 End Lemmas.
+
+Section Congruences.
+  Arguments label: clear implicits.
+  Context {E C: Type -> Type} {X: Type}.
+  Notation SP := (SS -> label E -> Prop).
+
+  (*| [lift] ignores trees and only looks at labels |*)
+  #[global] Instance proper_lift_equ: forall (p: label E -> Prop) R,
+      Proper (R ==> eq ==> iff) (@lift E C X p).
+  Proof.
+    unfold Proper, respectful, impl; cbn.      
+    intros p R x y EQ ? l <-; split; unfold lift; auto. 
+  Qed.
+
+  #[global] Instance proper_ax_equ (P: SP) {HasStuck: B0 -< C} :
+    Proper (@equ E C X X eq ==> eq ==> iff) (ax P).
+  Proof.
+    unfold Proper, respectful, impl; cbn.      
+    intros x y EQ ? l <-; split; intro NEXT;
+      unfold ax in *;
+      intros; eapply NEXT.
+    - now rewrite EQ.
+    - now rewrite <- EQ.
+  Qed.
+
+  #[global] Instance proper_ex_equ (P: SP) {HasStuck: B0 -< C} :
+    Proper (@equ E C X X eq ==> eq ==> iff) (ex P).
+  Proof.
+    unfold Proper, respectful, impl; cbn.      
+    intros x y EQ ? l <-; split; intro NEXT;
+      unfold ex in *; destruct NEXT as (l' & t' & NEXT);
+      do 2 eexists; intro TR; apply NEXT.
+    - now rewrite EQ.
+    - now rewrite <- EQ.
+  Qed.
+
+  #[global] Instance proper_ag_equ (P: SP) {HasStuck: B0 -< C}
+   {PrP: Proper (@equ E C X X eq ==> eq ==> iff) P}:
+    Proper (@equ E C X X eq ==> eq ==> iff) (ag P).
+  Proof.
+    unfold Proper, respectful, impl; cbn.
+    intros x y EQ ? l <-; split; intro G; step in G; inv G;
+      step; simpl; unfold agF; intros.
+    - rewrite EQ in H.
+      split; auto.
+      intros t' l' TR.
+      rewrite <- EQ in TR.
+      now apply H0.
+    - rewrite <- EQ in H.
+      split; auto.
+      intros t' l' TR.
+      rewrite EQ in TR.
+      now apply H0.
+  Qed. 
+
+  #[global] Instance proper_eg_equ (P: SP) {HasStuck: B0 -< C}
+   {PrP: Proper (@equ E C X X eq ==> eq ==> iff) P}:
+    Proper (@equ E C X X eq ==> eq ==> iff) (eg P).
+  Proof.
+    unfold Proper, respectful, impl; cbn.
+    intros x y EQ ? l <-; split; intro G; step in G; inv G;
+      step; simpl; unfold egF; intros.
+    - rewrite EQ in H.
+      split; auto.
+      destruct H0 as (t' & l' & TR).
+      exists t', l'. 
+      now rewrite EQ in TR.
+    - rewrite <- EQ in H.
+      split; auto.
+      destruct H0 as (t' & l' & TR).
+      exists t', l'.
+      now rewrite <- EQ in TR.
+  Qed.
+  
+End Congruences.
