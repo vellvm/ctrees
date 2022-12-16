16cce8ba7c8914cda6094b230b631331b4fa249b
Lef Ioannidis <elefthei@seas.upenn.edu>
2022-12-16 09:19:13 -0500

    Progress

--- theories/Interp/Uid.v
+++ theories/Interp/Uid.v
@@ -36,57 +36,54 @@ Local Open Scope fin_vector_scope.
   
 Set Implicit Arguments.
 
-Module Uid.
+(*| Unique thread identifiers |*)
+Notation uid := fin.
+(*| Thread IDs are a product of an effect [E] with a [uid] |*)
+Variant Uid (n: nat) (E: Type -> Type) : Type -> Type :=
+  | Frame {X} (e: E X) (i: uid n): Uid n E X.
 
-  (*| Unique thread identifiers |*)
-  Notation uid := fin.
-  (*| Thread IDs are a product of an effect [E] with a [uid] |*)
-  Variant Uid (n: nat) (E: Type -> Type) : Type -> Type :=
-    | Frame {X} (e: E X) (i: uid n): Uid n E X.
+Arguments Frame {n} {E} {X}.
 
-  Arguments Frame {n} {E} {X}.
+Definition single{E: Type -> Type}: E ~> Uid 1 E :=
+  fun R e => Frame e F1.
 
-  Definition single{E: Type -> Type}: E ~> Uid 1 E :=
-    fun R e => Frame e F1.
+Definition forget{n: nat}{E: Type -> Type}: Uid n E ~> E :=
+  fun R e => match e return (E _) with Frame e i => e end.
 
-  Definition forget{n: nat}{E: Type -> Type}: Uid n E ~> E :=
-    fun R e => match e return (E _) with Frame e i => e end.
-  
-  (* Every ctree with no thread identifiers can be injected in the single-threaded ctree *)
-  Definition inj_single {E C: Type -> Type}{X: Type}(t: ctree E C X): ctree (Uid 1 E) C X :=
-    translate single t.
+(* Every ctree with no thread identifiers can be injected in the single-threaded ctree *)
+Definition inj_single {E C: Type -> Type}{X: Type}(t: ctree E C X): ctree (Uid 1 E) C X :=
+  translate single t.
 
-  Section N.
-    Context {n: nat}.
-    (* Can forget thread identifiers in order to interpret trees *)
-    Definition inj_forget {E C: Type -> Type}{X: Type}(t: ctree (Uid n E) C X): ctree E C X :=
-      translate forget t.
+Section N.
+  Context {n: nat}.
+  (* Can forget thread identifiers in order to interpret trees *)
+  Definition inj_forget {E C: Type -> Type}{X: Type}(t: ctree (Uid n E) C X): ctree E C X :=
+    translate forget t.
 
-    Definition fold_n {E B M : Type -> Type}
-               {FM : Functor M} {MM : Monad M} {IM : MonadIter M}
-               (h : E ~> M) (g : bool -> B ~> M) :
-      ctree (Uid n E) B ~> M := fold (fun R e => @h R (forget e)) g.
+  Definition fold_n {E B M : Type -> Type}
+             {FM : Functor M} {MM : Monad M} {IM : MonadIter M}
+             (h : E ~> M) (g : bool -> B ~> M) :
+    ctree (Uid n E) B ~> M := fold (fun R e => @h R (forget e)) g.
 
-    Definition interp_n {E B M : Type -> Type}
-               {FM : Functor M} {MM : Monad M} {IM : MonadIter M} {BM : MonadBr B M}
-               (h : E ~> M) := fold_n h (fun b _ c => mbr b c).
+  Definition interp_n {E B M : Type -> Type}
+             {FM : Functor M} {MM : Monad M} {IM : MonadIter M} {BM : MonadBr B M}
+             (h : E ~> M) := fold_n h (fun b _ c => mbr b c).
 
-  End N.    
+End N.    
 
-  Arguments fold_n {n E B M FM MM IM} h g [T].
-  Arguments interp_n {n E B M FM MM IM BM} h [T].
+Arguments fold_n {n E B M FM MM IM} h g [T].
+Arguments interp_n {n E B M FM MM IM BM} h [T].
 
-  (** Now some ETL *)
-  Section Strans.
-    Context {C: Type -> Type} {X: Type} (S: Type) (n: nat) `{HasStuck: B0 -< C}.
-    Notation T := (ctree (Uid n (stateE S)) C X).
-    Notation SS := (@Trans.SS (Uid n (stateE S)) C X).    
-    Notation SP := (S -> SS -> Prop).
+(*| A transition relation on Uid (state) |*)
+Section Strans.
+  Context {C: Type -> Type} {X: Type} (S: Type) (n: nat) `{HasStuck: B0 -< C}.
+  Notation SS := (@Trans.SS (Uid n (stateE S)) C X).    
+  Notation SP := (S -> SS -> Prop).
 
-    (*| Finitely many [tau] and [obs Get s] labels, before getting a [obs (Put s') tt] |*)
-    Definition strans(i: uid n)(s s': S): srel SS SS :=
-      (cup (trans tau) (trans (obs (Frame (@Get S) i) s)))^* ⋅ trans (obs (Frame (Put s') i) tt).
-  End Strans.
+  (*| Finitely many [tau] and [obs Get s] labels, before getting a [obs (Put s') tt] |*)
+  Definition strans(i: uid n)(s s': S): srel SS SS :=
+    (cup (trans tau) (trans (obs (Frame (@Get S) i) s)))^* ⋅ trans (obs (Frame (Put s') i) tt).
+End Strans.
 
-End Uid.
+
 
--- theories/Logic/Etl.v
+++ theories/Logic/Etl.v
@@ -24,6 +24,9 @@ From ExtLib Require Import
 From Coinduction Require Import
      rel coinduction tactics.
 
+From RelationAlgebra Require Import
+     rel srel.
+
 Set Implicit Arguments.
 
 (*| A logic with explicit [uid n] path quantifiers, see [Interp/Uid.v] |*)
@@ -31,16 +34,15 @@ Module Etl.
 
   Section ETL.
     Context {C: Type -> Type} {X: Type} (S: Type) (n: nat) `{HasStuck: B0 -< C}.
-    Notation T := (ctree (Uid n (stateE S)) C X).
-    Notation SS := (@Trans.SS (Uid n (stateE S)) C X).    
-    Notation SP := (S -> SS -> Prop).
-
-    (*| Finitely many [tau] and [obs Get s] labels, before getting a [obs (Put s') tt] |*)
-    Definition strans(i: uid n)(s s': S): srel SS SS :=
-      (cup (trans tau) (trans (obs (Frame (@Get S) i) s)))^* ⋅ trans (obs (Frame (Put s') i) tt).
+    (** ctree (Uid n (stateE S)) C X *)
+    Notation SS := (ctree (Uid n (stateE S)) C X).
+    Notation SP := (SS -> S -> Prop).
 
     Definition lift(p: S -> Prop): SP :=
-      fun s _ => p s.
+      fun _ s => p s.
+
+    Definition Top := lift (fun _ => True).
+    Definition Bot := lift (fun _ => False).
 
     (* Propositional *)
     Definition eimpl: SP -> SP -> SP :=
@@ -55,19 +57,70 @@ Module Etl.
     Definition enot: SP -> SP :=
       fun a t l => not (a t l).    
   
-    (** exists[i] s, s |= next p *)
+    (* exists[i] s, s |= next p *)
     Definition iex(i: uid n)(p: SP): SP :=
-      fun s t =>
-        exists s' t', strans i s s' t t' /\ p s' t'.
+      fun t s =>
+        exists t' s', strans i s s' t t' /\ p t' s'.
 
-    (** forall[i] s, s |= next p *)
+    (* forall[i] s, s |= next p *)
     Definition iax(i: uid n)(p: SP): SP :=
-      fun s t =>
-        forall s' t', strans i s s' t t' -> p s' t'.
+      fun t s =>
+        forall t' s', strans i s s' t t' -> p t' s'.
 
-    (** exists[i] s, s |= eventually p *)
-    Definition iex(i: uid n)(p: SP): SP :=
-      fun s t => (exists s' t', strans i s s' t t' /\ p s' t').
+    (* exists[i] strong until *)
+    Inductive ieu(i: uid n)(p q: SP): SP :=
+    | MatchE: forall t s,      (* Match state [t] and label [l] *)
+        q t s -> ieu i p q t s
+    | StepE: forall t s,       (* Matches all next states [t'] *)
+        p t s ->
+        (exists s' t', strans i s s' t t' /\
+                    ieu i p q t' s') ->
+        ieu i p q t s.
+
+    (* Forall[i] strong until *)
+    Inductive iau(i: uid n)(p q: SP): SP :=
+    | MatchA: forall t s,      (* Match state [t] and label [l] *)
+        q t s -> iau i p q t s
+    | StepA: forall t s,       (* Matches all next states [t'] *)
+        p t s ->
+        (forall s' t', strans i s s' t t' ->
+                    iau i p q t' s') ->
+        iau i p q t s.
+    
+    (* Forall[i] finally *)
+    Definition iaf i p := (iau i Top p).
+  
+    (* Exists finally *)
+    Definition ief i p := (ieu i Top p).
+
+    (*| Coinductives |*)  
+    (* Forall global *)
+    Definition iagF (R: SP -> SP): SP -> SP :=
+      fun (p: SP) t l =>
+        p t l /\
+          (forall t' l', trans l' t t' -> R p t' l').
+
+  (* Exists global *)
+  Definition egF (R: SP -> SP): SP -> SP :=
+    fun (p: SP) t l =>
+      p t l /\
+      (exists t' l', trans l' t t' /\ R p t' l').
+
+  Hint Constructors eu au: core.
+  Hint Unfold lift cand cor cimpl cnot af ef ax ex agF egF: core.
+    
+  Program Definition ag_: mon (SP -> SP) := {| body := agF  |}.
+  Next Obligation.
+    destruct H0; split; eauto.
+  Qed.
+
+  Program Definition eg_: mon (SP -> SP) := {| body := egF  |}.  
+  Next Obligation.    
+    destruct H0 as (A & t' & l' & Htr & ?); split; eauto.
+  Qed.
+
+  Definition ag := (gfp (@ag_)).
+  Definition eg := (gfp (@eg_)).
 
 End Etl.
 
