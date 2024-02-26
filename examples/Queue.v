00c0a7e5ecd6f9c9913b2a9385f43f745ca91515
Lef Ioannidis <elefthei@seas.upenn.edu>
2024-02-25 21:28:27 -0500

    Observation is simpler (only care for Pop)

--- examples/Queue.v
+++ examples/Queue.v
@@ -83,26 +83,16 @@ Section QueueEx.
                          end
                  end).
 
-  Variant qview :=
-    | QPop (h: S)
-    | QPush (h: S).
-
-  Definition h_queueE: queueE S ~> stateT (list S) (ctree (writerE qview)) :=
+  Definition h_queueE: queueE S ~> stateT (list S) (ctree (writerE S)) :=
     h_writerA
       (* queue semantics *)
       (h_state h_queueE_sem)
-      (* queue instrumentation *)
+      (* queue instrumentation, observe [Pop] results *)
       (fun (e: queueE S) (v: encode e) (_: list S) =>
-         match e return encode e -> option qview with
-         | Pop => fun x: option S => option_map QPop x
-         | Push x => fun _ => Some (QPush x)
+         match e return encode e -> option S with
+         | Pop => fun x: option S => x
+         | Push x => fun _ => None
          end v).
-
-  Lemma list_app_contra: forall (s: S),
-      ~ (exists ts : list S, nil = ts ++ [s]).
-  Proof.
-    intros * Hv; destruct Hv, x; cbn in *; inv H.
-  Qed.
 
   Lemma list_app_cons: forall (h s: S) ts hs,
       h :: ts = hs ++ [s] ->
@@ -117,14 +107,14 @@ Section QueueEx.
   Opaque entailsF.
   Theorem drain_af_pop: forall (s: S) q,
       <( {interp_state h_queueE drain (q ++ [s])}, Pure |=
-         AF finishW \v l, v = QPop s /\ l = @nil S )>.
+         AF finishW \v l, v = s /\ l = @nil S )>.
   Proof.
     intros.
     apply af_iter_state_list
       with (Ri:=fun '(tt) w l =>
                   not_done w /\
                   match w with
-                  | Obs (Log (QPop s')) tt =>
+                  | Obs (Log s') tt =>
                       match l with
                       | nil => s = s'
                       | h :: ts => exists hs, h:: ts = hs ++ [s]
@@ -142,7 +132,7 @@ Section QueueEx.
     destruct l as [|h ts], Hw as [Hd Hw].
     - (* l = [] *)
       inv Hd; cbn.
-      + now apply list_app_contra in Hw.
+      + destruct Hw, x; cbn in *; inv H.
       + rewrite bind_ret_l, bind_tau.
         apply af_tau.
         rewrite interp_state_ret, bind_ret_l.
@@ -151,8 +141,7 @@ Section QueueEx.
         next; left.
         apply ax_finish.
         do 2 eexists; intuition.
-        destruct e, v, q0; subst; auto.
-        now apply list_app_contra in Hw.
+        destruct e, v; subst; auto.
     - (* l = h :: ts *)
       cbn.
       rewrite bind_trigger, bind_vis.      
@@ -169,28 +158,22 @@ Section QueueEx.
         apply ax_finish; cbn; destruct x; intuition. 
         inv Hd.
         * destruct Hw as [[|h' ts'] Hw];
-            apply list_app_cons in Hw as (-> & ->); cbn.
-          -- auto.
-          -- destruct ts'; cbn; intuition.
-             ++ now (exists nil).
-             ++ now (exists (s0 :: ts')).
-        * destruct e, q0, v, Hw; apply list_app_cons in H; destruct x, H; subst; cbn; auto.
-          all: destruct x; cbn; intuition; [now (exists nil) | now (exists (s1 :: x))].
+            apply list_app_cons in Hw as (-> & ->); cbn; auto.
+          destruct ts'; cbn; intuition.
+          -- now (exists nil).
+          -- now (exists (s0 :: ts')).
+        * destruct e, v, Hw; apply list_app_cons in H; destruct x, H; subst; cbn; auto.
+          destruct x; cbn; intuition.
+          -- now (exists nil).
+          -- now (exists (s2 :: x)).
   Qed.       
 
-
-  (*
-  forever p x |= AF now φ
-  ----------------------
-  forever p x |= WG WF now φ
-   *)
-  
-  (*| Even though [rotate] does not terminate, we eventually get [a] |*)
-  Theorem rotate_af_pop: forall (a b: S) (Hneq: a <> b) q w,
-      <( {interp_state instr_queueE (rotate b) (q ++ [a])}, w |=
-         AF vis {fun '(Log v) 'tt => exists l, v = VPop l a})>.
+  Theorem rotate_af_pop: forall (t f: S) (Htf: t <> f) q,
+      <( {interp_state h_queueE (rotate f) (q ++ [t])}, Pure |=
+         AF finishW \v l, v = t /\ Forall (eq f) l )>.
   Proof.
     intros.
+    Print rotate.
   Admitted.
 End QueueEx.
   
