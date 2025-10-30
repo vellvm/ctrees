850ca8846d6ded6c1c0fab63d8d59c945c04a9ef
Yannick <y.zakowski@gmail.com>
2025-10-30 11:08:25 +0100

    The family of bind lemmas. Need to think about the proper instance now that we have two kind of states

--- theories/Eq/SSim.v
+++ theories/Eq/SSim.v
@@ -116,7 +116,7 @@ Variant eq2 {E} : forall [X Y : Type], E X -> E Y -> rel X Y :=
 Hint Resolve Eq1 : trans.
 Hint Resolve Eq2 : trans.
 
-Definition Leq {E} (X : Type) : lrel E E X X :=
+Definition Leq {E} {X : Type} : lrel E E X X :=
   {|
     RR   := eq ;
     Rask := eq1 ;
@@ -140,6 +140,7 @@ Ltac edestruct5 H := edestruct H as (? & ? & ? & ? & ?).
 
 Definition lequiv {E F X Y} : rel (lrel E F X Y) (lrel E F X Y) :=
   fun L1 L2 => RR L1 == RR L2 /\ Rask L1 == Rask L2 /\ Rrcv L1 == Rrcv L2.
+
 #[global] Instance lequiv_equivalence {E F X Y} : Equivalence (@lequiv E F X Y).
 Proof.
   constructor.
@@ -208,10 +209,12 @@ Definition ssim {E F C D X Y} L :=
 Module SSimNotations.
 
   Infix "≲" := (ssim Leq) (at level 70).
+  Notation "t (≲ [ Q ] ) u" := (ssim (Lvrel Q) t u) (at level 79).
   Notation "t (≲ Q ) u" := (ssim Q t u) (at level 79).
-  Notation "t '[≲' R ']' u" := (ss R (` _) t u) (at level 90, only printing).
-  Notation "t '[≲]' u" := (ss Leq (` _) t u) (at level 90, only printing).
 
+  Notation "t '[≲]' u" := (ss Leq (` _) t u) (at level 90, only printing).
+  Notation "t '[≲' [ R ] ']' u" := (ss (Lvrel R) (` _) t u) (at level 90, only printing).
+  Notation "t '[≲' R ']' u" := (ss R (` _) t u) (at level 90, only printing).
 End SSimNotations.
 
 Import SSimNotations.
@@ -345,7 +348,7 @@ Section ssim_heterogenous_theory.
   Proof.
     intros t t' tt' u u' uu'; cbn; intros.
     rewrite tt' in H0. apply H in H0 as (l' & ? & ? & ? & ?).
-    do 2 eexists; eauto. rewrite uu'. eauto.
+    ex2; eauto. rewrite uu'. eauto.
   Qed.
 
   #[global] Instance equ_ss_closed_ctx {r} :
@@ -353,7 +356,7 @@ Section ssim_heterogenous_theory.
   Proof.
     intros t t' tt' u u' uu'; cbn; intros.
     rewrite <- tt' in H0. apply H in H0 as (l' & ? & ? & ? & ?).
-    do 2 eexists; eauto. rewrite <- uu'. eauto.
+    ex2; eauto. rewrite <- uu'. eauto.
   Qed.
 
 (*|
@@ -412,20 +415,20 @@ Section bind.
   Arguments label: clear implicits.
   Obligation Tactic := idtac.
 
-  Context {E F C D: Type -> Type} {X X' Y Y': Type}
-    (L : lrel E F X' Y')
-    (SS: rel X Y).
 (*|
 Specialization of [bind_ctx] to a function acting with [ssim] on the bound value,
 and with the argument (pointwise) on the continuation.
 |*)
   Lemma bind_chain_gen
+    {E F C D: Type -> Type} {X X' Y Y': Type}
+    (L : lrel E F X' Y')
+    (SS: rel X Y)
     {R : Chain (@ss E F C D X' Y' L)} :
     forall (t : ctree E C X) (t' : ctree F D Y)
       (k : X -> ctree E C X') (k' : Y -> ctree F D Y'),
       ssim (upd_Lrel L SS) t t' ->
-      (forall x y, SS x y -> elem R (k x) (k' y)) ->
-      elem R (bind t k) (bind t' k').
+      (forall x y, SS x y -> ` R (k x) (k' y)) ->
+      ` R (bind t k) (bind t' k').
   Proof.
     apply tower.
     - intros ? INC ? ? ? ? tt' kk' ? ?.
@@ -478,128 +481,84 @@ and with the argument (pointwise) on the continuation.
         erewrite <- trans_val_inv'; eauto.
   Qed.
 
-End bind.
+(*|
+Specialization: equality on external calls, equality everywhere
+|*)
+  Lemma bind_chain E C D X Y X' Y'
+    (RR : rel X' Y') (SS : rel X Y)
+    {R : Chain (@ss E E C D X' Y' (Lvrel RR))} :
+    forall (t1 : ctree E C X) (t2: ctree E D Y)
+      (k1 : X -> ctree E C X') (k2 : Y -> ctree E D Y'),
+      t1 (≲[SS]) t2 ->
+      (forall x y, SS x y -> `R (k1 x) (k2 y)) ->
+      `R (t1 >>= k1) (t2 >>= k2).
+  Proof.
+    intros.
+    eapply bind_chain_gen; eauto.
+  Qed.
+
+  Lemma bind_chain_eq E C X X'
+    {R : Chain (@ss E E C C X' X' Leq)} :
+    forall (t1 t2 : ctree E C X)
+      (k1 k2 : X -> ctree E C X'),
+      t1 ≲ t2 ->
+      (forall x, `R (k1 x) (k2 x)) ->
+      `R (t1 >>= k1) (t2 >>= k2).
+  Proof.
+    intros.
+    eapply bind_chain_gen; eauto.
+    intros ??<-; auto.
+  Qed.
 
 (*|
-Specializing the congruence principle for [≲]
+Specializations to the gfp
 |*)
-Lemma ssim_clo_bind_gen E F C D X Y X' Y' L (RR : rel X' Y') Rask Rrcv (SS : rel X Y) L'
-  (HL  : good_rel L  RR Rask Rrcv)
-  (HL' : good_rel L' SS Rask Rrcv)
-  (t1 : ctree E C X) (t2: ctree F D Y)
-  (k1 : X -> ctree E C X') (k2 : Y -> ctree F D Y'):
-  ssim L' t1 t2 ->
-  (forall x y, SS x y -> ssim L (k1 x) (k2 y)) ->
-  ssim L (t1 >>= k1) (t2 >>= k2).
-Proof.
-  intros.
-  eapply bind_chain_gen; eauto.
-Qed.
+  Lemma ssim_bind_gen E F C D X Y X' Y'
+    L (SS : rel X Y) 
+    (t1 : ctree E C X) (t2: ctree F D Y)
+    (k1 : X -> ctree E C X') (k2 : Y -> ctree F D Y'):
+    t1 (≲ upd_Lrel L SS) t2 ->
+    (forall x y, SS x y -> k1 x (≲ L) k2 y) ->
+    t1 >>= k1 (≲ L) t2 >>= k2.
+  Proof.
+    intros.
+    eapply bind_chain_gen; eauto.
+  Qed.
 
-Lemma ssim_clo_bind {E F C D: Type -> Type} {X Y X' Y': Type} {L : rel (label E) (label F)}
-      (R0 : rel X Y)
-      (t1 : ctree E C X) (t2: ctree F D Y)
-      (k1 : X -> ctree E C X') (k2 : Y -> ctree F D Y'):
-  t1 (≲update_val_rel L R0) t2 ->
-  (forall x y, R0 x y -> k1 x (≲L) k2 y) ->
-  t1 >>= k1 (≲L) t2 >>= k2.
-Proof.
-  intros.
-  eapply bind_chain_gen; eauto using update_val_rel_correct.
-Qed.
+  Lemma ssim_bind E C D X Y X' Y'
+    (RR : rel X' Y') (SS : rel X Y) 
+    (t1 : ctree E C X) (t2: ctree E D Y)
+    (k1 : X -> ctree E C X') (k2 : Y -> ctree E D Y'):
+    t1 (≲ [SS]) t2 ->
+    (forall x y, SS x y -> k1 x (≲ [RR]) k2 y) ->
+    t1 >>= k1 (≲ [RR]) t2 >>= k2.
+  Proof.
+    intros.
+    eapply bind_chain_gen; eauto.
+  Qed.
 
-Lemma ssim_clo_bind_eq {E C D: Type -> Type} {X X': Type}
-      (t1 : ctree E C X) (t2: ctree E D X)
-      (k1 : X -> ctree E C X') (k2 : X -> ctree E D X'):
-  t1 ≲ t2 ->
-  (forall x, k1 x ≲ k2 x) ->
-  t1 >>= k1 ≲ t2 >>= k2.
-Proof.
-  intros.
-  eapply bind_chain_gen; eauto.
-  - apply update_val_rel_eq.
-  - intros; subst. apply H0.
-Qed.
+  Lemma ssim_bind_eq {E C D: Type -> Type} {X X': Type}
+    (t1 : ctree E C X) (t2: ctree E D X)
+    (k1 : X -> ctree E C X') (k2 : X -> ctree E D X'):
+    t1 ≲ t2 ->
+    (forall x, k1 x ≲ k2 x) ->
+    t1 >>= k1 ≲ t2 >>= k2.
+  Proof.
+    intros.
+    eapply ssim_bind; eauto.
+    intros ?? ->; auto.
+  Qed.
 
+End bind.
 
-
-#[global] Instance update_val_rel_Lequiv {E F X Y X' Y'} :
-  Proper (Lequiv X' Y' ==> weq ==> Lequiv X Y) (@update_val_rel E F X Y).
-Proof.
-  cbn. red. intros.
-  red in H. split; intro.
-  - inv H3.
-    + left. apply H0. auto.
-    + right; auto.
-      apply H; auto; now apply wf_val_nonval.
-  - inv H3.
-    + left. apply H0. auto.
-    + right; auto.
-      apply H; auto; now apply wf_val_nonval.
-Qed.
-
-#[global] Instance is_update_val_rel_Lequiv {E F X Y X' Y'} :
-  Proper (Lequiv X' Y' ==> weq ==> Lequiv X Y ==> flip impl) (@is_update_val_rel E F X Y).
-Proof.
-  cbn -[weq]. red. intros. red in H2. subst. now rewrite H, H0, H1.
-Qed.
-
-Theorem update_val_rel_update_val_rel {E F X0 X1 Y0 Y1}
-    (L : rel (label E) (label F)) (R0 : rel X0 Y0) (R1 : rel X1 Y1) :
-  update_val_rel (update_val_rel L R0) R1 == update_val_rel L R1.
-Proof.
-  split; intro.
-  - destruct H.
-    + now constructor.
-    + destruct H1. { exfalso. now apply H. }
-      constructor; auto.
-  - destruct H.
-    + now constructor.
-    + constructor; auto.
-      constructor; auto.
-Qed.
-
-Theorem is_update_val_rel_update_val_rel_eq {E X Y Z} :
-  forall (R : rel X Y),
-  @Lequiv E E Z Z (@update_val_rel E E Z Z (update_val_rel eq R) eq) eq.
-Proof.
-  intros. rewrite update_val_rel_update_val_rel.
-  now rewrite update_val_rel_eq.
-Qed.
-
-#[global] Instance Symmetric_update_val_rel {E X} L R0 :
-  Symmetric L ->
-  Symmetric R0 ->
-  Symmetric (@update_val_rel E E X X L R0).
-Proof.
-  cbn. intros. destruct H1; constructor; auto.
-Qed.
-
-#[global] Instance Transitive_update_val_rel :
-  forall {E X} (L : relation (label E)) (R0 : relation X),
-  Transitive L ->
-  Transitive R0 ->
-  Transitive (update_val_rel L R0).
-Proof.
-  red. intros. destruct y.
-  - inv H1. inv H2. constructor; auto. etransitivity; eassumption.
-  - inv H1. inv H2. constructor; auto. etransitivity; eassumption.
-  - inv H1; [| exfalso; etrans].
-    inv H2; [| exfalso; etrans].
-    invert. constructor. eauto.
-Qed.
-
-Definition lift_val_rel {E X Y} := @update_val_rel E E X Y eq.
 (*|
 And in particular, we can justify rewriting [≲] to the left of a [bind].
 
 NOTE: we shouldn't have to impose [eq] to the right.
 |*)
 #[global] Instance ssim_bind_chain {E C X Y}
-  {R : Chain (@ss E E C C Y Y eq)} :
-  Proper (ssim eq ==>
-            (pointwise_relation _ (elem R)) ==>
-            (elem R)) (@bind E C X Y).
+  {R : Chain (@ss E E C C Y Y Leq)} :
+  Proper (ssim Leq ==> (pointwise_relation _ (` R)) ==> ` R) (bind E C X Y).
 Proof.
   repeat intro; eapply bind_chain_gen; eauto.
   - apply update_val_rel_eq.
