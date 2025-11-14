68f1dcc477ef295afc6f9fd0f5a956098986dea5
Nicolas Chappe <nicolas.chappe@ens-lyon.fr>
2025-11-14 12:21:42 +0100

    Remove 8.20 workaround

--- theories/Interp/FoldCTree.v
+++ theories/Interp/FoldCTree.v
@@ -319,7 +319,6 @@ Counter-example showing that interp does not preserve sbisim in the general case
 
 Module CounterExample.
 
-  Unset Automatic Proposition Inductives.
   Inductive VoidE : Type -> Type :=
   | voidE : VoidE void.
 
